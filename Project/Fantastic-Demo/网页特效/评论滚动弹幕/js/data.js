15a1d25063bb4d26e6c8d157126b5507d873c1a4
W-Qing <scorpio971028@gmail.com>
2019-09-28 23:26:20 +0800

    update Fantastic-Demo project

---
+++ Project/Fantastic-Demo/网页特效/评论滚动弹幕/js/data.js
@@ -0,0 +1,9 @@
+
+var data = [
+	{href : 'http://fy.035k.com',text : '我的博客http://fy.035k.com'},
+	{href : 'http://www.baidu.com',text : '百度'},
+	{href : 'http://www.17sucai.com',text : 'jquery插件网'},
+	{href : '',text : '苏打绿可根据历史'},
+	{href : '',text : '管理数据管理'},
+	{href : '',text : '另外今年光缆和并购水晶宫老师'}
+]
