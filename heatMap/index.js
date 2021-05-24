323722683c415db18e35f1ae3f2418bb7c0a0edf
W-Qing <scorpio971028@gmail.com>
2021-05-24 16:26:34 +0800

    update heatmap

--- heatMap/index.js
+++ heatMap/index.js
@@ -2,8 +2,9 @@ const { Scene, Sprite, Group, Label, Ring, Path, Rect, Gradient } = spritejs
 const container = document.querySelector('#stage')
 const scene = new Scene({container, width: 1900, height: 900, mode: 'stickyTop'})
 
-// 配置项
+// 1. 配置默认项
 const defaultHeatMapConfig = {
+  // 渐变色
   gradient: {
     0.25: "blue",
     0.45: "lime",
@@ -15,17 +16,12 @@ const defaultHeatMapConfig = {
   radius: 100,
 }
 
-// 为每个数据点设置一个从中心向外灰度渐变的圆
+// 2. 将每个数据点映射为一个从中心向外灰度渐变的圆
 const createShadowTpl = () => {
   const { min, max, radius } = defaultHeatMapConfig
-  // 数据格式，包括坐标信息与权重值 0~100
-  // const heatMapData = [
-  //   { x: 871, y: 277, value: 25 },
-  //   { x: 638, y: 375, value: 97 },
-  //   { x: 773, y: 190, value: 71 },
-  // ]
 
   // 生成100个随机坐标点数据
+  // 数据格式，包括坐标信息与权重值 0~100  { x: 871, y: 277, value: 25 }
   const heatMapData = Object.keys(Array.from({ length: 100 })).map(item => {
     return {
       x: Math.floor(Math.random()*(1500-50+1)+50),
@@ -37,8 +33,9 @@ const createShadowTpl = () => {
   // 生成灰度的单点元素
   const singleSprite = heatMapData.map(data => {
     return new Sprite().attr({
-      // spriteJs api render by webGL
+      // spriteJs api, render by webGL
       bgcolor: new Gradient({
+        // 根据 vector(三维向量)参数个数不同，分别创建LinearGradient和RadialGradient
         vector: [radius, radius, 0, radius, radius, radius],
         colors: [
           { offset: 0, color: 'rgba(0,0,0,1)'},
@@ -53,7 +50,7 @@ const createShadowTpl = () => {
   return singleSprite
 }
 
-// 创建渐变色卡
+// 3. 创建渐变色卡
 const createColordata = () => {
   const colourBar = document.createElement('canvas')
   const cCtx = colourBar.getContext('2d')
@@ -72,18 +69,21 @@ const createColordata = () => {
 // handleEvent:false 不派发事件，优化性能
 heatmaplayer = scene.layer('heatmaplayer', { contextType: '2d', handleEvent: false})
 
-// 上色
+// 4. 上色
 const renderHeatMap = () => {
   heatmaplayer.removeAllChildren()
   // spriteJS默认的layer，append放入元素后会自动渲染
   heatmaplayer.append(...createShadowTpl())
-  // 在prepareRender时拿到该层layer的ImageData，对其重新上色
+  // ⚠️ 在prepareRender时拿到该层layer的ImageData，对其重新上色
   heatmaplayer.prepareRender.then(() => {
     const colorData = createColordata()
     const paintCtx = heatmaplayer.canvas.getContext('2d')
     const paintData = paintCtx.getImageData(0, 0, 1900, 900)
+    // paintData: ImageData对象中存储着canvas对象真实的像素数据
     const { data } = paintData
-    // rgb
+    // data: Uint8ClampedArray类型的一维数组，包含着RGBA格式的整型数据，范围在0至255之间（包括255）
+    
+    // 灰度数据可以使用Uint8ClampedArray来运算，不一定非得画出灰色的canvas来获取rgb数据
     for (let i = 0; i < data.length; i++) {
       const value = data[i]
       if (value) {
